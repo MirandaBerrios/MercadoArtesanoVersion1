50ef15f43e95146e1f7bbcb261dd6e89cebdf26d
MirandaBerrios <73406595+MirandaBerrios@users.noreply.github.com>
2021-04-18 16:35:56 -0400

    Final version
    We have a final work to the team 1 to "Programación web" firts semester 2021

---
+++ js/animacion.js
@@ -0,0 +1,7 @@
+function change_image(image){
+    var image_container = document.getElementById("main-image");
+    
+    
+    image_container.src = image.src;
+    
+    }
---
+++ readme.txt
@@ -0,0 +1,5 @@
+~ Cambiar el diseno para que sea responsivo 
+
+~ anadir imagenes, videos y disenos finales
+
+~ mejorar paleta de colores y concepto del diseno
